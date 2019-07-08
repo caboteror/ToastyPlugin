39d5b27f4eec0d74564c839356675fc37077e109
t1cbotero <cbotero@todo1.com>
2019-07-08 12:29:28 -0500

    Test cargando

--- src/ios/CDVUIInAppBrowser.h
+++ src/ios/CDVUIInAppBrowser.h
@@ -68,13 +68,13 @@
 
 }
 
-@property (nonatomic, strong) IBOutlet UIWebView* webView;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* closeButton;
 @property (nonatomic, strong) IBOutlet UILabel* addressLabel;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* backButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* forwardButton;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
 @property (nonatomic, strong) IBOutlet UIToolbar* toolbar;
+@property (nonatomic, strong) IBOutlet UIWebView* webView;
 
 @property (nonatomic, weak) id <CDVScreenOrientationDelegate> orientationDelegate;
 @property (nonatomic, weak) CDVUIInAppBrowser* navigationDelegate;
