8a56864b0e1085e22ea17f2ade7f3849a01e8e58
t1cbotero <cbotero@todo1.com>
2019-07-08 15:43:57 -0500

    Test cargando

--- src/ios/CDVUIInAppBrowser.m
+++ src/ios/CDVUIInAppBrowser.m
@@ -795,7 +795,7 @@ static CDVUIInAppBrowser* instance = nil;
     self.closeButton = title != nil ? [[UIBarButtonItem alloc] initWithTitle:title style:UIBarButtonItemStyleBordered target:self action:@selector(close)] : [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(close)];
     self.closeButton.enabled = YES;
     // If color on closebutton is requested then initialize with that that color, otherwise use initialize with default
-    self.closeButton.tintColor = colorString != nil ? [self colorFromHexString:colorString] : [UIColor colorWithRed:60.0 / 255.0 green:136.0 / 255.0 blue:230.0 / 255.0 alpha:1];
+    self.closeButton.tintColor = colorString != nil ? [self colorFromHexString:colorString] : [UIColor colorWithRed:200.0 / 255.0 green:150.0 / 255.0 blue:150.0 / 255.0 alpha:1];
 
     NSMutableArray* items = [self.toolbar.items mutableCopy];
     [items replaceObjectAtIndex:buttonIndex withObject:self.closeButton];
